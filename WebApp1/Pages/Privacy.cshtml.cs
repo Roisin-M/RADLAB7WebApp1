63e6bc9b474b65d222a9014394e5dd000824de6f
S00223865 <S00223865@S-6M053S3>
2024-12-05 13:01:45 +0000

    Add project files.

---
+++ WebApp1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp1.Pages
+{
+    //to test identity, add authorize annotation
+    //this brings user to login page when they try to access privacy page
+    [Authorize]
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
